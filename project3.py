d03c4b7575fa801d291638d43de84cbb1cfbf5c0
nikolpetkova10 <petkovaniki123@gmail.com>
2026-01-15 12:54:57 +0200

    Add favorite sport selection feature
    Added a select box for favorite sport and updated session state.

--- project3.py
+++ project3.py
@@ -23,7 +23,10 @@ if "sports" not in st.session_state:
 st.subheader("Избери любими неща")
 
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
+sport = st.selectbox("Любим спорт:", list(st.session_state.colors.keys()))
+
 color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
+sport = st.selectbox("Любим спорт:", list(st.session_state.colors.keys()))
 
 if st.button("Запази избора"):
  st.session_state.colors[color] += 1 
