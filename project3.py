3ca303ec312d71aee6cf7a766f32edf290ed4c24
nikolpetkova10 <petkovaniki123@gmail.com>
2026-01-15 12:52:28 +0200

    Refactor initialization and display of session data

--- project3.py
+++ project3.py
@@ -4,26 +4,46 @@ import pandas as pt
 st.title("📊 Любими неща - класна анкета")
 
  # Инициализация на данните
-if "colors" not in st.session_state: st.session_state.colors = { "Червен": 0, "Син": 0, "Зелен": 0, "Жълт": 0 }
+if "colors" not in st.session_state:
+ st.session_state.colors = { 
+  "Червен": 0, 
+  "Син": 0,
+  "Зелен": 0,
+  "Жълт": 0 
+ }
 
-if "sports" not in st.session_state: st.session_state.sports = { "Футбол": 0, "Баскетбол": 0, "Волейбол": 0, "Плуване": 0 }
+if "sports" not in st.session_state: 
+ st.session_state.sports = {
+  "Футбол": 0,
+  "Баскетбол": 0,
+  "Волейбол": 0, 
+  "Плуване": 0 
+ }
 
 st.subheader("Избери любими неща")
 
-color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys())) sports_df = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
+color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
+color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys()))
 
-
-color = st.selectbox("Любим цвят:", list(st.session_state.colors.keys())) sports_df = st.selectbox("Любим спорт:", list(st.session_state.sports.keys()))
-
-if st.button("Запази избора"): st.session_state.colors[color] += 1 st.session_state.sports[sport] += 1 st.success("Изборът е записан!")
+if st.button("Запази избора"):
+ st.session_state.colors[color] += 1 st.session_state.sports[sport] += 1 
+ st.success("Изборът е записан!")
 
 st.divider()
 
 st.subheader("☑️ Резултати")
 
 # Графика за цветовете
-st.write("Любими цветове") colors_df = pd.DataFrame.from_dict( st.session_state.colors, orient="index", columns=["Брой"] ) st.bar_chart(colors_df)
+st.write("Любими цветове") 
+colors_df = pd.DataFrame.from_dict( 
+ st.session_state.colors, orient="index", columns=["Брой"] 
+) 
+st.bar_chart(colors_df)
 
 # Графика за спортовете
-st.write("Любими спортове") sports_df = pd.DataFrame.from_dict( st.session_state.sports, orient="index", columns=["Брой"] ) st.bar_chart(sports_df)
+st.write("Любими спортове")
+sports_df = pd.DataFrame.from_dict(
+ st.session_state.sports, orient="index", columns=["Брой"]
+) 
+st.bar_chart(sports_df)
 
